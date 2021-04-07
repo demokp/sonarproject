ed49503fb1939b8286f7b17e703037c4df031646
root <root@sonarqube.us-central1-a.c.boxwood-bee-309804.internal>
2021-04-07 10:15:39 +0000

    comit

---
+++ HelloAgain.java
@@ -0,0 +1,10 @@
+public class HelloAgain{
+// this is my comment
+// this is agin a comment
+// is thsi 
+	public static void main(String[] args) {
+//		System.out.println("Hello all");
+		LOGGER.log("Hello")
+	}
+// this is final comment
+}
